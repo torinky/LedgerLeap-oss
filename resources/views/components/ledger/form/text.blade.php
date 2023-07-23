c218df38a5f6dc9915b5787ee4a5a8ac84483449
torinky <k_tori@me.com>
2023-07-23 09:15:49 +0900

    auto width ajust

--- resources/views/components/ledger/form/text.blade.php
+++ resources/views/components/ledger/form/text.blade.php
@@ -1,9 +1,51 @@
-{{--<input type="hidden" name="content[{{$columnDefine->id}}]" value=" ">--}}
+<input
+    wire:model.lazy="content.{{$columnDefine->id}}" id="content[{{$columnDefine->id}}]"
+    class="adjustWidth input input-bordered" name="content[{{$columnDefine->id}}]"
+    value="{{$this->content[$columnDefine->id] ?? ''}}"
+>
 
-<input type="text" wire:model.lazy="content.{{$columnDefine->id}}" id="content[{{$columnDefine->id}}]"
-       class="input input-bordered"
-       name="content[{{$columnDefine->id}}]" value="{{$this->content[$columnDefine->id] ?? ''}}"
-       style="width:{{mb_strlen($this->content[$columnDefine->id])<10?10:mb_strlen($this->content[$columnDefine->id])+2}}em"
-       onInput="if(this.value.length>10){this.style.width = (this.value.length+2)+'em'}"
-/>
+@once
+    @push('scripts')
+        <script>
+            document.addEventListener('livewire:load', function () {
+                const inputs = document.querySelectorAll('input.adjustWidth');
 
+                function getWidthOfInput(input) {
+                    const temp = document.createElement('span');
+                    temp.textContent = input.value || input.placeholder;
+                    temp.style.position = 'absolute';
+                    temp.style.visibility = 'hidden';
+                    document.body.appendChild(temp);
+                    const width = temp.offsetWidth + 50;
+                    document.body.removeChild(temp);
+                    return width;
+                }
+
+                function adjustInputWidth() {
+                    inputs.forEach((input) => {
+                        const width = getWidthOfInput(input);
+                        input.style.width = width + 'px';
+                    });
+                }
+
+                window.adjustWidth = function (event) {
+                    const input = event.target;
+                    const width = getWidthOfInput(input);
+                    input.style.width = width + 'px';
+                    console.log(input.style.width);
+                };
+
+                adjustInputWidth();
+
+                window.addEventListener("keydown", function (event) {
+                    event.preventDefault();
+                    window.adjustWidth(event);
+                });
+
+                Livewire.hook('message.processed', () => {
+                    adjustInputWidth();
+                });
+            });
+        </script>
+    @endpush
+@endonce
