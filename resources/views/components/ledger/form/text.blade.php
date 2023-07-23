77ae4d2c0deef7dc483cfad03b2d4bc680c184e9
torinky <k_tori@me.com>
2023-07-23 09:19:02 +0900

    auto width ajust

--- resources/views/components/ledger/form/text.blade.php
+++ resources/views/components/ledger/form/text.blade.php
@@ -1,26 +1,37 @@
+<!-- text.blade.php -->
+<!--
+    テキスト入力フォームの幅を動的に調整するためのLivewireコンポーネントのビューファイルです。
+    $columnDefine->idに基づいて動的なフォームを作成します。
+    フォームの入力内容はcontent[$columnDefine->id]というLivewireのプロパティで管理されます。
+-->
 <input
-    wire:model.lazy="content.{{$columnDefine->id}}" id="content[{{$columnDefine->id}}]"
-    class="adjustWidth input input-bordered" name="content[{{$columnDefine->id}}]"
-    value="{{$this->content[$columnDefine->id] ?? ''}}"
+    wire:model.lazy="content.{{ $columnDefine->id }}"
+    id="content[{{$columnDefine->id}}]"
+    class="adjustWidth input input-bordered"
+    name="content[{{$columnDefine->id}}]"
+    value="{{ $this->content[$columnDefine->id] ?? '' }}"
 >
 
 @once
     @push('scripts')
         <script>
             document.addEventListener('livewire:load', function () {
+                // 動的なフォームの幅を調整するために.input.adjustWidthクラスを持つ全ての入力要素を取得します
                 const inputs = document.querySelectorAll('input.adjustWidth');
 
+                // 入力要素の幅を計算する関数
                 function getWidthOfInput(input) {
                     const temp = document.createElement('span');
                     temp.textContent = input.value || input.placeholder;
                     temp.style.position = 'absolute';
                     temp.style.visibility = 'hidden';
                     document.body.appendChild(temp);
-                    const width = temp.offsetWidth + 50;
+                    const width = temp.offsetWidth + 50; // テキストの幅に余裕を持たせるために50pxを加えます
                     document.body.removeChild(temp);
                     return width;
                 }
 
+                // 全ての入力要素の幅を調整する関数
                 function adjustInputWidth() {
                     inputs.forEach((input) => {
                         const width = getWidthOfInput(input);
@@ -28,20 +39,24 @@
                     });
                 }
 
+                // 入力内容が変更された際に呼び出される関数
                 window.adjustWidth = function (event) {
                     const input = event.target;
                     const width = getWidthOfInput(input);
                     input.style.width = width + 'px';
-                    console.log(input.style.width);
+                    // console.log(input.style.width);
                 };
 
+                // ページがロードされた時に入力フォームの幅を初期化
                 adjustInputWidth();
 
+                // キーが押されたときに入力フォームの幅を調整
                 window.addEventListener("keydown", function (event) {
                     event.preventDefault();
                     window.adjustWidth(event);
                 });
 
+                // Livewireメッセージが処理された際に入力フォームの幅を更新
                 Livewire.hook('message.processed', () => {
                     adjustInputWidth();
                 });
