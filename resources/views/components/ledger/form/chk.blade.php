c9392e095215de2ad4d5d81a5b557ba22c78bad3
torinky <k_tori@me.com>
2023-07-23 10:34:26 +0900

    fix checkbox

--- resources/views/components/ledger/form/chk.blade.php
+++ resources/views/components/ledger/form/chk.blade.php
@@ -1,13 +1,15 @@
 @foreach ($columnDefine->options as $option)
-    <div class="form-control">
-        <label for="content[{{$columnDefine->id}}][{{$option}}]" class="label cursor-pointer">
-            <span class="label-text">{{$option}}</span>
-            <input type="checkbox" wire:model="content.{{$columnDefine->id}}.{{$option}}"
+    <div class="form-control mr-5">
+        {{-- 各オプションのラベルとチェックボックスを表示する --}}
+        <label for="content[{{$columnDefine->id}}][{{$option}}]" class="label cursor-pointer space-x-2">
+            <input type="checkbox"
+                   wire:model="content.{{$columnDefine->id}}.{{$option}}" {{-- Livewireの双方向データバインディングを使用 --}}
                    id="content[{{$columnDefine->id}}][{{$option}}]"
                    name="content[{{$columnDefine->id}}][{{$option}}]" value="{{$option}}"
-                   @php( (isset($lthis->content) && is_array($this->content[$columnDefine->id]) && in_array($option, $this->content[$columnDefine->id]??[]) ) ? 'checked="checked"' : '')
+                   @php( (isset($this->content) && is_array($this->content[$columnDefine->id]) && in_array($option, $this->content[$columnDefine->id]??[]) ) ? 'checked="checked"' : '') {{-- チェック済みの場合はチェックボックスを選択状態にする --}}
                    class="input-bordered checkbox"
             />
+            <span class="label-text">{{$option}}</span> {{-- オプションの名前を表示 --}}
         </label>
     </div>
 @endforeach
