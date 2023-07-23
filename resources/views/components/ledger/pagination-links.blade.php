4fe7528500e71a63b548c471c135bd28d212202d
torinky <k_tori@me.com>
2023-07-23 10:32:54 +0900

    fix pagination

--- resources/views/components/ledger/pagination-links.blade.php
+++ resources/views/components/ledger/pagination-links.blade.php
@@ -1,33 +1,51 @@
 <div>
+    {{-- ページネーションがある場合にのみ表示 --}}
     @if ($paginator->hasPages())
-        <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-center my-5">
+        {{-- ページネーションナビゲーション --}}
+        <nav role="navigation" aria-label="ページネーション" class="flex justify-center my-5">
             <div class="join">
+                {{-- 現在のページが最初のページでない場合に表示 --}}
                 @if (!$paginator->onFirstPage())
-                    <button wire:click="gotoPage(1)" class="join-item btn "><i
+                    {{-- 最初のページに移動するボタン --}}
+                    <button wire:click="gotoPage({{1}})" class="join-item btn"><i
                             class="fa-sharp fa-solid fa-backward-step"></i></button>
+                    {{-- 前のページに移動するボタン --}}
                     <button wire:click="previousPage" class="join-item btn"><i
                             class="fa-solid fa-play fa-flip-horizontal px-3"></i></button>
                 @endif
-                @if($paginator->currentPage()>2 )
-                    @for($i=$paginator->currentPage()-3;$i<$paginator->currentPage();$i++)
-                        @if($i>0)
-                            <button wire:click="gotoPage({{ $i }})" class="join-item btn ">{{ $i }}</button>
+
+                {{-- 現在のページが2ページ目以降の場合に表示 --}}
+                @if($paginator->currentPage() > 1)
+                    {{-- 現在のページより前の3つのページボタンを表示 --}}
+                    @for($i = $paginator->currentPage() - 3; $i < $paginator->currentPage(); $i++)
+                        {{-- 1より大きいページ番号のみ表示 --}}
+                        @if($i > 0)
+                            <button wire:click="gotoPage({{ $i }})"
+                                    class="join-item btn">{{ $i }}</button>
                         @endif
                     @endfor
                 @endif
-                <button class="join-item btn btn-active">{{$paginator->currentPage()}}
-                    / {{ $this->lastPage() }}</button>
+
+                {{-- 現在のページを表示 (アクティブなスタイルを適用) --}}
+                <button class="join-item btn btn-active">
+                    {{ $paginator->currentPage() }} / {{ $this->lastPage() }}
+                </button>
+
+                {{-- 次のページが存在する場合に表示 --}}
                 @if ($paginator->hasMorePages())
-                    @for($i=($paginator->currentPage()+1);$i<=$this->lastPage() && ($i<$paginator->currentPage()+4);$i++)
-                        <button wire:click="gotoPage({{ $i }})" class="join-item btn">{{ $i }}</button>
+                    {{-- 現在のページより後の3つのページボタンを表示 --}}
+                    @for($i = ($paginator->currentPage() + 1); $i <= $this->lastPage() && ($i < $paginator->currentPage() + 4); $i++)
+                        <button wire:click="gotoPage({{ $i }})"
+                                class="join-item btn">{{ $i }}</button>
                     @endfor
-                    <button wire:click="nextPage" class="join-item btn"><i
+                    {{-- 次のページに移動するボタン --}}
+                    <button wire:click="nextPage()" class="join-item btn"><i
                             class="fa-solid fa-play px-3"></i></button>
+                    {{-- 最後のページに移動するボタン --}}
                     <button wire:click="gotoPage({{ $this->lastPage() }})" class="join-item btn "><i
                             class="fa-sharp fa-solid fa-forward-step"></i></button>
                 @endif
             </div>
-
         </nav>
     @endif
 </div>
