208c40fca6b4d0621411043428f804d1d8d715a2
torinky <k_tori@me.com>
2023-07-24 10:26:24 +0900

    fix pagination

--- app/Http/Livewire/Ledger/RecordsTable.php
+++ app/Http/Livewire/Ledger/RecordsTable.php
@@ -37,42 +37,53 @@ class RecordsTable extends Component
     public $totalRecords;
 
     /**
-     * 初回リクエストの時はこちらで初期化される
+     * コンポーネントが初めてリクエストされた時に実行される初期化処理
+     *
      * @param SearchRequest $request
      * @return void
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
-    public function mount(searchRequest $request)
+    public function mount(SearchRequest $request)
     {
+        // 検索キーワードの初期化
         $search = $request->keyword();
         if (empty($this->search) && !empty($search)) {
             $this->search = $search ?? session()->get('search') ?? '';
         }
-        $this->updateKeyworrdsAndTags($this->search);
+        $this->updateKeywordsAndTags($this->search);
 
+        // 現在のフォルダーIDを初期化
         $this->currentFolderId = $request->folderId();
+
+        // もし台帳IDが指定されていれば、選択済みリストに追加
         if ($request->ledgerDefineId()) {
             $this->selectedLedgerDefineIds = [$request->ledgerDefineId()];
         }
+
+        // フォルダーアセットを準備
         $this->prepareFolderAsset();
     }
 
+    /**
+     * 列のソートを行う
+     *
+     * @param string $columnName
+     * @return void
+     */
     public function sort($columnName)
     {
         $this->orderBy = $columnName;
 
-        if ($this->orderAsc) {
-            $this->orderAsc = false;
-        } else {
-            $this->orderAsc = true;
-        }
+        // 現在のソート順をトグル
+        $this->orderAsc = !$this->orderAsc;
 
         $this->render();
     }
 
     /**
-     * ajaxリクエストの処理はこちら
+     * コンポーネントの表示を更新する
+     *
      * @return Application|Factory|View
      */
     public function render()
@@ -80,20 +91,19 @@ class RecordsTable extends Component
         // checkboxのキーはサーバー側で変えるとブラウザに正しく反映されなくなる
         $this->selectedFolderIds = array_filter($this->selectedFolderIds, 'strlen');
 
-        $this->updateKeyworrdsAndTags($this->search);
+        $this->updateKeywordsAndTags($this->search);
 
         $descendantFolderIds = [];
         foreach ($this->selectedFolderIds as $selectedFolderId) {
             $descendantFolderIds = array_merge(
                 Folder::whereDescendantOf($selectedFolderId)
-                    ->pluck('id')->toArray()
-                , $descendantFolderIds
+                    ->pluck('id')->toArray(),
+                $descendantFolderIds
             );
         }
 
-        $displayLedgerDefines = LedgerDefine::whereIn('folder_id',
-            array_merge($this->selectedFolderIds, $descendantFolderIds)
-        )
+        // 表示対象の台帳を取得
+        $displayLedgerDefines = LedgerDefine::whereIn('folder_id', array_merge($this->selectedFolderIds, $descendantFolderIds))
             ->orWhereIn('id', $this->selectedLedgerDefineIds)
             ->searchTags($this->tags)
             ->with('folder')
@@ -103,45 +113,55 @@ class RecordsTable extends Component
 
         $breadcrumbsPerLedgerDefine = [];
         foreach ($displayLedgerDefines as $displayLedgerDefine) {
+            // 台帳ごとのパンくずリストを準備
             $breadcrumbsPerLedgerDefine[$displayLedgerDefine->id] = $displayLedgerDefine->folder->parents();
             $breadcrumbsPerLedgerDefine[$displayLedgerDefine->id][] = $displayLedgerDefine->folder;
         }
 
-
+        // 表示対象の台帳に紐づく仕訳データを取得
         $ledgerRecords = Ledger::whereIn('ledger_define_id', $searchTargetLedgerDefineIds)
-            ->search(implode(' ', $this->keywords))->contentsFilter($this->filter)
+            ->search(implode(' ', $this->keywords))
+            ->contentsFilter($this->filter)
             ->with('define.folder');
+
+        // 仕訳データの総数を取得
         $this->totalRecords = $ledgerRecords->count();
 
-        return view('livewire.ledger.records-table'
-            , [
-                'ledgerRecords' =>
-                /*                    Ledger::whereIn('ledger_define_id', $searchTargetLedgerDefineIds)
-                                        ->search(implode(' ', $this->keywords))->contentsFilter($this->filter)
-                                        ->with('define.folder')*/
-                    $ledgerRecords
-                        ->orderBy('ledger_define_id', 'asc')
-                        ->orderBy($this->orderBy, $this->orderAsc ? 'asc' : 'desc')
-                        ->simplePaginate($this->perPage),
-                'breadcrumbsPerLedgerDefine' => $breadcrumbsPerLedgerDefine,
-            ]
-        );
-    }
-
-    public function contentsFilter($defineId, $columnNo, $word)
-    {
-//        dd($defineId,$columnNo,$word);
-        $this->defineId = $defineId;
-        $this->filter[$columnNo] = $word;
-        $this->render();
-
+        return view('livewire.ledger.records-table', [
+            'ledgerRecords' =>
+            /*                    Ledger::whereIn('ledger_define_id', $searchTargetLedgerDefineIds)
+                                    ->search(implode(' ', $this->keywords))->contentsFilter($this->filter)
+                                    ->with('define.folder')*/
+                $ledgerRecords
+                    ->orderBy('ledger_define_id', 'asc')
+                    ->orderBy($this->orderBy, $this->orderAsc ? 'asc' : 'desc')
+                    ->simplePaginate($this->perPage),
+            'breadcrumbsPerLedgerDefine' => $breadcrumbsPerLedgerDefine,
+        ]);
     }
 
     /**
+     * コンテンツのフィルタリングを行う
+     *
+     * @param int $defineId
+     * @param int $columnNo
+     * @param string $word
+     * @return void
+     */
+    public function contentsFilter($defineId, $columnNo, $word)
+    {
+        $this->defineId = $defineId;
+        $this->filter[$columnNo] = $word;
+        $this->render();
+    }
+
+    /**
+     * 入力されたテキストからキーワードとタグを更新する
+     *
      * @param string $rawInputText
      * @return void
      */
-    private function updateKeyworrdsAndTags($rawInputText)
+    private function updateKeywordsAndTags($rawInputText)
     {
         $text = mb_convert_kana($rawInputText, 'askV', 'UTF-8');
         $text = preg_replace('/\s+/u', ' ', $text);
@@ -156,36 +176,61 @@ class RecordsTable extends Component
         }
     }
 
-
+    /**
+     * 現在のフォルダーを変更する
+     *
+     * @param int $newFolderId
+     * @return void
+     */
     public function changeCurrentFolder($newFolderId)
     {
+        // フォルダーIDを更新
         $this->currentFolderId = $newFolderId;
+
+        // フォルダーアセットを再準備
         $this->prepareFolderAsset();
 
+        // ツリーコンポーネントに現在のフォルダーIDの変更を通知
         $this->emit('currentFolderChangedByMain', $this->currentFolderId);
-
     }
 
+    /**
+     * ツリーコンポーネントから通知を受け取り、現在のフォルダーを変更する
+     *
+     * @param int $newFolderId
+     * @return void
+     */
     public function currentFolderChangedByTree($newFolderId)
     {
+        // フォルダーIDを更新
         $this->currentFolderId = $newFolderId;
 
+        // フォルダーアセットを再準備
         $this->prepareFolderAsset();
     }
 
-    /*
-       public function toggleLedgerDefineOpen($targetLedgerDefineId)
-        {
-            if (in_array($targetLedgerDefineId,$this->selectedLedgerDefineIds)) {
-                $this->selectedLedgerDefineIds = collect($this->selectedLedgerDefineIds)->reject(function ($item) use ($targetLedgerDefineId) {
-                    return ($item === $targetLedgerDefineId)||($item===false);
-                })->toArray();
-            }else{
-                $this->selectedLedgerDefineIds[]=$targetLedgerDefineId;
-            }
-        }
-    */
     /**
+     * 台帳を開閉する（コメントアウト済みのコード）
+     *
+     * @param int $targetLedgerDefineId
+     * @return void
+     */
+    /*
+    public function toggleLedgerDefineOpen($targetLedgerDefineId)
+    {
+        if (in_array($targetLedgerDefineId, $this->selectedLedgerDefineIds)) {
+            $this->selectedLedgerDefineIds = collect($this->selectedLedgerDefineIds)->reject(function ($item) use ($targetLedgerDefineId) {
+                return ($item === $targetLedgerDefineId) || ($item === false);
+            })->toArray();
+        } else {
+            $this->selectedLedgerDefineIds[] = $targetLedgerDefineId;
+        }
+    }
+    */
+
+    /**
+     * フォルダーアセットを準備する
+     *
      * @return void
      */
     public function prepareFolderAsset(): void
@@ -206,18 +251,41 @@ class RecordsTable extends Component
         }
     }
 
+    /**
+     * 台帳の選択状態をトグルする
+     *
+     * @param int $ledgerDefineId
+     * @return void
+     */
     public function toggleLedgerDefineId($ledgerDefineId)
     {
         if (in_array($ledgerDefineId, $this->selectedLedgerDefineIds)) {
+            // 選択済みの場合、リストから削除
             $this->selectedLedgerDefineIds = array_values(array_diff($this->selectedLedgerDefineIds, [$ledgerDefineId]));
         } else {
+            // 選択されていない場合、リストに追加
             $this->selectedLedgerDefineIds[] = $ledgerDefineId;
         }
     }
 
+    /**
+     * ページネーションの総ページ数を計算する
+     *
+     * @return int
+     */
     public function lastPage()
     {
         return ceil($this->totalRecords / $this->perPage);
     }
 
+    /**
+     * ページサイズが変更された際に、現在のページをリセットする
+     *
+     * @return void
+     */
+    public function updatingPerPage()
+    {
+        // perPageを変更した場合、currentPageを最初のページにリセットする
+        $this->resetPage();
+    }
 }
--- resources/views/components/ledger/pagination-links.blade.php
+++ resources/views/components/ledger/pagination-links.blade.php
@@ -1,3 +1,8 @@
+@php
+    $currentPage = $paginator->currentPage();
+    $lastPage = $this->lastPage();
+@endphp
+
 <div>
     {{-- ページネーションがある場合にのみ表示 --}}
     @if ($paginator->hasPages())
@@ -7,43 +12,51 @@
                 {{-- 現在のページが最初のページでない場合に表示 --}}
                 @if (!$paginator->onFirstPage())
                     {{-- 最初のページに移動するボタン --}}
-                    <button wire:click="gotoPage({{1}})" class="join-item btn"><i
-                            class="fa-sharp fa-solid fa-backward-step"></i></button>
+                    <button wire:click="gotoPage(1)" class="join-item btn">
+                        <i class="fa-sharp fa-solid fa-backward-step"></i>
+                    </button>
                     {{-- 前のページに移動するボタン --}}
-                    <button wire:click="previousPage" class="join-item btn"><i
-                            class="fa-solid fa-play fa-flip-horizontal px-3"></i></button>
+                    <button wire:click="previousPage" class="join-item btn">
+                        <i class="fa-solid fa-play fa-flip-horizontal px-3"></i>
+                    </button>
                 @endif
 
                 {{-- 現在のページが2ページ目以降の場合に表示 --}}
-                @if($paginator->currentPage() > 1)
-                    {{-- 現在のページより前の3つのページボタンを表示 --}}
-                    @for($i = $paginator->currentPage() - 3; $i < $paginator->currentPage(); $i++)
-                        {{-- 1より大きいページ番号のみ表示 --}}
-                        @if($i > 0)
-                            <button wire:click="gotoPage({{ $i }})"
-                                    class="join-item btn">{{ $i }}</button>
-                        @endif
-                    @endfor
-                @endif
+                @php
+                    $startPage = max($currentPage - 3, 1);
+                    $endPage = $currentPage;
+                @endphp
+
+                {{-- 現在のページより前の3つのページボタンを表示 --}}
+                @for ($i = $startPage; $i < $endPage; $i++)
+                    <button wire:click="gotoPage({{ $i }})" class="join-item btn">{{ $i }}</button>
+                @endfor
 
                 {{-- 現在のページを表示 (アクティブなスタイルを適用) --}}
                 <button class="join-item btn btn-active">
-                    {{ $paginator->currentPage() }} / {{ $this->lastPage() }}
+                    {{ $currentPage }} / {{ $lastPage }}
                 </button>
 
                 {{-- 次のページが存在する場合に表示 --}}
                 @if ($paginator->hasMorePages())
                     {{-- 現在のページより後の3つのページボタンを表示 --}}
-                    @for($i = ($paginator->currentPage() + 1); $i <= $this->lastPage() && ($i < $paginator->currentPage() + 4); $i++)
-                        <button wire:click="gotoPage({{ $i }})"
-                                class="join-item btn">{{ $i }}</button>
+                    @php
+                        $startPage = $currentPage + 1;
+                        $endPage = min($currentPage + 4, $lastPage);
+                    @endphp
+
+                    @for ($i = $startPage; $i <= $endPage; $i++)
+                        <button wire:click="gotoPage({{ $i }})" class="join-item btn">{{ $i }}</button>
                     @endfor
+
                     {{-- 次のページに移動するボタン --}}
-                    <button wire:click="nextPage()" class="join-item btn"><i
-                            class="fa-solid fa-play px-3"></i></button>
+                    <button wire:click="nextPage" class="join-item btn">
+                        <i class="fa-solid fa-play px-3"></i>
+                    </button>
                     {{-- 最後のページに移動するボタン --}}
-                    <button wire:click="gotoPage({{ $this->lastPage() }})" class="join-item btn "><i
-                            class="fa-sharp fa-solid fa-forward-step"></i></button>
+                    <button wire:click="gotoPage({{ $lastPage }})" class="join-item btn">
+                        <i class="fa-sharp fa-solid fa-forward-step"></i>
+                    </button>
                 @endif
             </div>
         </nav>
