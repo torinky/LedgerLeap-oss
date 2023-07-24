58b8ee5c4fa65a0e8b15cded9c03a201fe238388
torinky <k_tori@me.com>
2023-07-24 10:47:11 +0900

    add textarea

--- app/Models/ColumnDefine.php
+++ app/Models/ColumnDefine.php
@@ -7,50 +7,82 @@ use RuntimeException;
 
 class ColumnDefine
 {
+    // 列の種類に関する設定
     static $useOptionsTypes = ['select', 'chk'];
     static $types = [
-        "number",
-        "text",
-        "chk",
-        "select",
-        "YMD",
-        'files',
+        "number",       // 数値型
+        "text",         // テキストボックス
+        "textarea",     // テキストエリア
+        "chk",          // チェックボックス
+        "select",       // セレクトボックス
+        "YMD",          // 日付（年月日）
+        'files',        // ファイル選択
     ];
-    public $id;
-    public $name;
-    public $type;
-    public $order;
-    public $useOptions;
-    public $options;
-    public $required;
-    public $sortBy;
-    public $doNotDuplicate;
 
+    // プロパティの定義
+    public $id;                 // ID
+    public $name;               // 列名
+    public $type;               // 列の種類
+    public $order;              // 列の表示順序
+    public $useOptions;         // オプション使用フラグ
+    public $options;            // オプション値の配列
+    public $required;           // 必須項目フラグ
+    public $doNotDuplicate;     // 重複不可フラグ
+    public $sortBy;             // ソート対象フラグ
 
+    /**
+     * コンストラクタ
+     *
+     * 引数の数に応じてオブジェクトによる初期化か値による初期化かを振り分ける
+     */
     function __construct()
     {
         $a = func_get_args();
         $i = func_num_args();
         if ($i == 1) {
-            call_user_func_array(array($this, '_constructByObject'), $a);
+            call_user_func_array(array($this, '__constructByObject'), $a);
             return;
         }
         if ($i > 1) {
-            call_user_func_array(array($this, '_constructByArgs'), $a);
+            call_user_func_array(array($this, '__constructByArgs'), $a);
             return;
         }
-        throw new Exception('invalid args');
+        throw new Exception('無効な引数');
     }
 
     /**
+     * オブジェクトによる初期化
+     *
+     * @param object $inObject
+     * @return void
+     */
+    public function __constructByObject($inObject)
+    {
+        $this->id = (int)$inObject->id;
+        $this->name = $inObject->name;
+        $this->type = $inObject->type;
+        $this->order = $inObject->order;
+        $this->initUseOptions();
+        $this->options = $inObject->options ?? [];
+        $this->required = $inObject->required;
+        $this->doNotDuplicate = $inObject->doNotDuplicate;
+        $this->sortBy = $inObject->sortBy;
+    }
+
+    /**
+     * 値による初期化
+     *
      * @param int $id
      * @param string $name
      * @param string $type
      * @param int $order
      * @param array $options
      * @param bool $required
+     * @param bool $doNotDuplicate
+     * @param bool $sortBy
+     * @return void
      */
-    public function _constructByArgs(
+    public function __constructByArgs(
         int    $id,
         string $name,
         string $type = 'text',
@@ -73,48 +105,22 @@ class ColumnDefine
     }
 
     /**
-     * @param object $inObject
-     * @return void
-     */
-    public function _constructByObject($inObject)
-    {
-        $this->id = (int)$inObject->id;
-        $this->name = $inObject->name;
-        $this->type = $inObject->type;
-        $this->order = $inObject->order;
-        $this->initUseOptions();
-        $this->options = $inObject->options ?? [];
-        $this->required = $inObject->required;
-        $this->doNotDuplicate = $inObject->doNotDuplicate;
-        $this->sortBy = $inObject->sortBy;
-    }
-
-    /**
+     * 列の種類を設定する
+     *
      * @param string $type
      */
     public function setType(string $type): void
     {
         if (!in_array($type, self::$types)) {
-            throw new RuntimeException('invalid type');
+            throw new RuntimeException('無効な列の種類');
         }
         $this->type = $type;
         $this->initUseOptions();
     }
 
-    /*    public function getRawObject(): object
-        {
-            return (object)[
-                'id' => $this->id,
-                'name' => $this->name,
-                'type' => $this->type,
-                'order' => $this->order,
-                'useOptions' => $this->useOptions,
-                'options' => $this->options,
-                'required' => $this->required
-            ];
-        }*/
-
     /**
+     * オプション使用フラグを初期化する
+     *
      * @return void
      */
     private function initUseOptions(): void
@@ -122,28 +128,21 @@ class ColumnDefine
         $this->useOptions = in_array($this->type, self::$useOptionsTypes);
     }
 
-
-    /*    public function __get($name)
-        {
-            return $this->$name ?? null;
-        }
-
-        public function __set($name, $value)
-        {
-            if (isset($this->$name)) {
-                $this->$name = $value;
-            }
-        }*/
-
+    /**
+     * 列の種類のラベルを取得する
+     *
+     * @return array
+     */
     static function typeLabels()
     {
         return [
-            "number" => __('auto numbering'),
-            "text" => __('text box'),
-            "chk" => __('check box'),
-            "select" => __('select box'),
-            "YMD" => __('date Y-M-D'),
-            "files" => __('select file'),
+            "number" => __('auto numbering'), // 自動採番
+            "text" => __('text box'), // テキストボックス
+            "textarea" => __('textarea'), // テキストエリア
+            "chk" => __('check box'), // チェックボックス
+            "select" => __('select box'), // セレクトボックス
+            "YMD" => __('date Y-M-D'), // 日付（年月日）
+            "files" => __('select file'), // ファイル選択
         ];
     }
 }
---
+++ resources/views/components/ledger/form/textarea.blade.php
@@ -0,0 +1,75 @@
+<!-- textarea.blade.php -->
+<!--
+    テキストエリアフォームの幅と高さを動的に調整するためのLivewireコンポーネントのビューファイルです。
+    $columnDefine->idに基づいて動的なフォームを作成します。
+    フォームの入力内容はcontent[$columnDefine->id]というLivewireのプロパティで管理されます。
+-->
+<textarea
+    wire:model.lazy="content.{{ $columnDefine->id }}"
+    id="content[{{$columnDefine->id}}]"
+    class="adjustWidth adjustHeight input input-bordered"
+    name="content[{{$columnDefine->id}}]"
+>
+{{ $this->content[$columnDefine->id] ?? '' }}
+</textarea>
+
+@once
+    @push('scripts')
+        <script>
+            // ドキュメントがロードされたときに実行する関数
+            document.addEventListener('livewire:load', function () {
+                // 幅と高さを調整するためのテキストエリア要素を取得
+                const textareas = document.querySelectorAll('textarea.adjustWidth.adjustHeight');
+
+                // テキストエリアの幅と高さを調整する関数
+                function adjustInputSize(input) {
+                    // テキストエリアの内容をクローンして幅を計算する
+                    const clone = input.cloneNode();
+                    clone.style.overflowY = 'hidden'; // クローンでのスクロールバー表示を無効に
+                    clone.style.height = 'auto'; // クローンの高さを自動調整
+                    clone.style.width = 'auto'; // クローンの幅を自動調整
+                    clone.style.whiteSpace = 'pre'; // クローンで改行を考慮
+
+                    // クローンを一時的に画面外に配置
+                    clone.style.position = 'absolute';
+                    clone.style.left = '-9999px';
+                    clone.style.top = '-9999px';
+                    document.body.appendChild(clone);
+
+                    // クローンの幅と高さを取得
+                    const width = clone.scrollWidth + 25; // テキストの幅に余裕を持たせるために25pxを加えます
+                    const height = clone.scrollHeight;
+
+                    // クローンを削除
+                    document.body.removeChild(clone);
+
+                    // テキストエリアの幅と高さを設定
+                    input.style.width = width + 'px';
+                    input.style.height = height + 'px';
+                }
+
+                // 各テキストエリアに対してイベントリスナーを設定
+                textareas.forEach((textarea) => {
+                    textarea.addEventListener('input', function (event) {
+                        adjustInputSize(textarea);
+                    });
+                });
+
+                // ページがロードされた際にすべてのテキストエリアの幅と高さを調整
+                function adjustAllInputSizes() {
+                    textareas.forEach((textarea) => {
+                        adjustInputSize(textarea);
+                    });
+                }
+
+                // ページがロードされた際にすべてのテキストエリアの幅と高さを初期化
+                adjustAllInputSizes();
+
+                // Livewireメッセージが処理された際にすべてのテキストエリアの幅と高さを更新
+                Livewire.hook('message.processed', () => {
+                    adjustAllInputSizes();
+                });
+            });
+        </script>
+    @endpush
+@endonce
