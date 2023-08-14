9e30b56ddee0eb37386baa9b13ffd707c6288aa5
torinky <k_tori@me.com>
2023-08-14 21:17:51 +0900

    add tree ledger count

--- resources/views/components/folder/tree.blade.php
+++ resources/views/components/folder/tree.blade.php
@@ -9,7 +9,7 @@
             <a wire:click="changeCurrentFolder({{$folder->id}})" @class(['bg-secondary-focus/30' => $folder->id == $currentFolderId])>
                 <i class="fa fa-folder-open"></i> {{ $folder->title }}
                 @if($folder->ledgerDefines->count()>0)
-                    <span class="badge badge-info text-info-content">{{ $folder->ledgerDefines->count() }}</span>
+                    <span class="badge badge-info">{{ $folder->ledgerDefines->count() }}</span>
                 @endif
             </a>
             @if($folder->children->isNotEmpty())
