964a0d8dd567983245e642f3c7404db4083bba21
torinky <k_tori@me.com>
2023-09-18 21:16:15 +0900

    tree refine

--- resources/views/components/folder/tree.blade.php
+++ resources/views/components/folder/tree.blade.php
@@ -4,10 +4,13 @@
     ])
 <ul>
     @foreach($folders as $folder)
-        <li>
-            {{--            <a href="{{route('ledgersByFolderId',['folderId'=>$folder->id])}}" @class(['bg-secondary-focus/30' => $folder->id == $currentFolderId])>--}}
+        <li class="{{$folder->id==1 ? 'root':''}}">
             <a wire:click="changeCurrentFolder({{$folder->id}})" @class(['bg-secondary-focus/30' => $folder->id == $currentFolderId])>
-                <i class="fa fa-folder-open"></i> {{ $folder->title }}
+                @if($folder->id==1)
+                    <i class="fas fa-home text-primary"></i>
+                @else
+                    <i class="fas fa-folder-open text-secondary"></i>
+                @endif {{ $folder->title }}
                 @if($folder->ledgerDefines->count()>0)
                     <span class="badge badge-info text-base-100"><i class="fas fa-book mr-2"></i> {{ $folder->ledgerDefines->count() }}</span>
                 @endif
