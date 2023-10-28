509f39197748f872bae3c687235e709ab43a9bcb
torinky <k_tori@me.com>
2023-10-28 19:55:05 +0900

    add folder count

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -19,6 +19,11 @@ class Folder extends Model
         return $this->hasMany(LedgerDefine::class);
     }
 
+    public function folders()
+    {
+        return $this->hasMany(Folder::class, 'parent_id');
+    }
+
     public function tag()
     {
         return $this->hasMany(Tag::class, 'ledger_define_id');
