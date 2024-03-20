327e1b96cfc24508805a9c75bc1bc8c6b7985e9d
torinky <k_tori@me.com>
2024-03-20 21:10:55 +0900

    refine detail view

--- resources/views/ledger/detail/table.blade.php
+++ resources/views/ledger/detail/table.blade.php
@@ -1,14 +1,16 @@
-<div class="container mx-auto prose lg:prose-xl">
+<div class="container mx-auto">
     {{--        @dd($ledgerRecord)--}}
     @if($ledgerRecord && $ledgerRecord->content && $ledgerRecord->define)
-        <h3 class="">
+        <div class="card bg-base-100 shadow-xl mt-10">
+            <h2 class="card-title">
             {{$ledgerRecord->define->title}}
-        </h3>
-        <table class="table table-zebra table-compact  table-fixed w-full">
+            </h2>
+            <div class="card-body">
+                <table class="table table-zebra table-compact table-hover table-fixed w-full">
             <tbody>
             @foreach($ledgerRecord->define->column_define as $cKey => $columnDefine)
                 <tr>
-                    <th class="w-1/6 break-words">
+                    <th class="w-1/3 lg:w-1/4 break-words">
                         {{$columnDefine->name}}
                     </th>
                     <td class="break-words">
@@ -18,5 +20,7 @@
             @endforeach
             </tbody>
         </table>
+            </div>
+        </div>
     @endif
 </div>
