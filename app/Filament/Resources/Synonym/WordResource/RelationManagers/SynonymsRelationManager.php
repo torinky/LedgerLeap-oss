1d935ae5aeb417a76e1cd22e4030374b184173fd
torinky <k_tori@me.com>
2025-01-05 20:40:16 +0900

    add page

---
+++ app/Filament/Resources/Synonym/WordResource/RelationManagers/SynonymsRelationManager.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources\Synonym\WordResource\RelationManagers;
+
+use App\Filament\Resources\Synonym\SynonymResource;
+use App\Models\Synonym\Word;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Collection;
+
+class SynonymsRelationManager extends RelationManager
+{
+    public static $primaryKey = 'wordid';
+
+    public static string $relationship = 'synonyms';
+
+    public static $resource = SynonymResource::class;
+
+    public static function getRelatedRecords(): Collection
+    {
+        $word = Word::find(request()->get('record'));
+
+        return $word->synonyms;
+    }
+
+    /*    public function getRelatedRecords(): \Illuminate\Database\Eloquent\Collection
+        {
+            return $this->ownerRecord->synonyms;
+        }*/
+
+    public function getHeading(): string
+    {
+        return "Synonyms of {$this->ownerRecord->word}";
+    }
+
+    //    protected static string $relationship = 'synonyms';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('synonym')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('synonym')
+            ->columns([
+                Tables\Columns\TextColumn::make('synonym'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
