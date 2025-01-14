ee41fc8f259399f0c01054324f90da88b6d777ec
torinky <k_tori@me.com>
2025-01-14 10:28:27 +0900

    fix form bug

--- resources/views/components/ledger/form/number.blade.php
+++ resources/views/components/ledger/form/number.blade.php
@@ -31,26 +31,50 @@
         "
     />
 @else
-    <x-mary-input
-        wire:model.blur="content.{{$columnDefine->id}}"
-        label="{{$columnDefine->name}}"
-        name="content[{{$columnDefine->id}}]"
-        placeholder="{{$columnDefine->name}}"
-        icon="{{$icon}}"
-        clearable
-        class="{{$class}}"
-        required="{{$columnDefine->required}}"
-        hint="{{$columnDefine->hint}}"
-        x-on:focus="
-        const opacityBlock = event.target.closest('.opacity-control-block');
-        opacityBlock.classList.add('opacity-100');
-        opacityBlock.classList.remove('opacity-50');
-        updateBackground('{{$columnDefine->id}}');
-        "
-        x-on:blur="
-        const opacityBlock = event.target.closest('.opacity-control-block');
-        opacityBlock.classList.add('opacity-50');
-        opacityBlock.classList.remove('opacity-100');
-        "
-    />
+    @if($columnDefine->required)
+        <x-mary-input
+            wire:model.blur="content.{{$columnDefine->id}}"
+            label="{{$columnDefine->name}}"
+            name="content[{{$columnDefine->id}}]"
+            placeholder="{{$columnDefine->name}}"
+            icon="{{$icon}}"
+            clearable
+            class="{{$class}}"
+            required="{{$columnDefine->required}}"
+            hint="{{$columnDefine->hint}}"
+            x-on:focus="
+            const opacityBlock = event.target.closest('.opacity-control-block');
+            opacityBlock.classList.add('opacity-100');
+            opacityBlock.classList.remove('opacity-50');
+            updateBackground('{{$columnDefine->id}}');
+            "
+            x-on:blur="
+            const opacityBlock = event.target.closest('.opacity-control-block');
+            opacityBlock.classList.add('opacity-50');
+            opacityBlock.classList.remove('opacity-100');
+            "
+        />
+    @else
+        <x-mary-input
+            wire:model.blur="content.{{$columnDefine->id}}"
+            label="{{$columnDefine->name}}"
+            name="content[{{$columnDefine->id}}]"
+            placeholder="{{$columnDefine->name}}"
+            icon="{{$icon}}"
+            clearable
+            class="{{$class}}"
+            hint="{{$columnDefine->hint}}"
+            x-on:focus="
+            const opacityBlock = event.target.closest('.opacity-control-block');
+            opacityBlock.classList.add('opacity-100');
+            opacityBlock.classList.remove('opacity-50');
+            updateBackground('{{$columnDefine->id}}');
+            "
+            x-on:blur="
+            const opacityBlock = event.target.closest('.opacity-control-block');
+            opacityBlock.classList.add('opacity-50');
+            opacityBlock.classList.remove('opacity-100');
+            "
+        />
+    @endif
 @endif
