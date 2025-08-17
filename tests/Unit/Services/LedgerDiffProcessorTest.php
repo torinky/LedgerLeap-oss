b650864d53ca7a1b319329163d0233380ef311e1
torinky <k_tori@me.com>
2025-08-17 13:28:54 +0900

    fix : test

--- tests/Unit/Services/LedgerDiffProcessorTest.php
+++ tests/Unit/Services/LedgerDiffProcessorTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit\Services;
+namespace tests\Unit\Services;
 
 use App\Models\Ledger;
 use App\Models\LedgerDefine;
--- tests/Unit/Traits/WorkflowActionsTest.php
+++ tests/Unit/Traits/WorkflowActionsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Traits;
+namespace tests\Unit\Traits;
 
 use App\Enums\WorkflowStatus;
 use App\Models\Ledger;
