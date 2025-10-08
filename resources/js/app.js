4e7a3624eab22c367c3f8f7e9d78cbb929782cc7
torinky <k_tori@me.com>
2025-10-08 21:31:38 +0900

    refactor(ui): streamline expandable-content component with reusable logic
    - Replaced inline Alpine.js logic with centralized `expandableContent` module.
    - Updated blade view to utilize `expandableContent` for better maintainability.
    - Registered `expandableContent` with Alpine.js during initialization in `app.js`.

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,8 @@ import './bootstrap';
 // import '@fortawesome/fontawesome-free/js/fontawesome.js';
 import '@fortawesome/fontawesome-free/js/all'
 
+import expandableContent from './components/expandable-content.js';
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('expandableContent', expandableContent)
+});
--- resources/views/components/expandable-content.blade.php
+++ resources/views/components/expandable-content.blade.php
@@ -1,43 +1,26 @@
 @props([
     'content' => '',
-    'maxHeight' => '6rem', // デフォルトの最大高さ（約4-5行）
+    'maxHeight' => '6rem',
     'showMoreText' => __('ledger.show_more'),
     'showLessText' => __('ledger.show_less'),
 ])
 
 <div
-    x-data="{
-        expanded: false,
-        showToggle: false,
-        checkOverflow() {
-            const content = this.$refs.content;
-            if (content) {
-                // コンテンツの実高さが max-height よりも大きい場合にトグルを表示
-                // style.maxHeight は '6rem' のような文字列なので、pixel値に変換して比較する
-                const maxHeightInPixels = parseFloat(getComputedStyle(content).maxHeight);
-                this.showToggle = content.scrollHeight > maxHeightInPixels;
-            }
-        }
-    }"
-    x-init="
-        $nextTick(() => checkOverflow());
-        window.addEventListener('resize', () => checkOverflow());
-    "
+    x-data="expandableContent({ maxHeight: '{{ $maxHeight }}' })"
     class="relative"
 >
     <div
         x-ref="content"
         :class="{ 'overflow-hidden': !expanded }"
-        :style="expanded ? '' : (showToggle ? `max-height: {{ $maxHeight }}; -webkit-mask-image: linear-gradient(to bottom, black calc(100% - 3rem), transparent 100%); mask-image: linear-gradient(to bottom, black calc(100% - 3rem), transparent 100%);` : `max-height: {{ $maxHeight }}`)"
+        :style="contentStyle"
         {{ $attributes->merge(['class' => 'transition-all duration-300']) }}
     >
         {!! $content !!}
     </div>
 
-    <!-- Show more/less ボタン -->
     <button
         x-show="showToggle"
-        @click="expanded = !expanded"
+        @click="toggle()"
         type="button"
         class="btn btn-sm btn-ghost w-full mt-2 gap-2"
     >
